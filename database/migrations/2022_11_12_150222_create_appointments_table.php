892391416a57160d53502b30e7aa8cb531c927c0
harunurR <harunurrashid97.ctg@gmail.com>
2022-11-16 13:18:35 +0600

    a

--- database/migrations/2022_11_12_150222_create_appointments_table.php
+++ database/migrations/2022_11_12_150222_create_appointments_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('appointments', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('patient_id')->unsigned()->nullable();
+            $table->unsignedBigInteger('patient_id')->nullable();
             $table->foreign('patient_id')->references('patient_id')->on('patients')->onDelete('cascade');
-            $table->integer('employee_id')->unsigned()->nullable();
+            $table->unsignedBigInteger('employee_id')->nullable();
             $table->foreign('employee_id')->references('employee_id')->on('employees')->onDelete('cascade');
             $table->string('serial');
             $table->string('problem');
