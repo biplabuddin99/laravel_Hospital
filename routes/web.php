3038c325a677dc523d74afb769c69c34b9b40169
Rabib <jrony538>
2022-11-21 14:18:12 +0600

    web

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\PatientAdmitController;
+use App\Http\Controllers\ShiftController;
+use App\Http\Controllers\BirthController;
 
 /*
 |--------------------------------------------------------------------------
