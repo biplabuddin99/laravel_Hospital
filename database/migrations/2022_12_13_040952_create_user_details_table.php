caf1da01128b9180ee4decebd80dda435b8308fe
biplab990 <biplabuddin990@gmail.com>
2022-12-20 22:36:19 +0600

    ok

--- database/migrations/2022_12_13_040952_create_user_details_table.php
+++ database/migrations/2022_12_13_040952_create_user_details_table.php
@@ -17,16 +17,10 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->unsignedBigInteger('role_id');
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
-            $table->string('name')->nullable();
-            $table->string('email')->nullable();
-            $table->string('phone')->nullable();
             $table->string('gender')->nullable();
             $table->string('birth_date')->nullable();
 			$table->unsignedBigInteger('blood_id')->nullable();
 			$table->foreign('blood_id')->references('id')->on('bloods')->onDelete('cascade');
-            $table->string('picture')->nullable();
             $table->string('address')->nullable();
             $table->integer('status')->default(1);
             $table->integer('created_by')->nullable();
